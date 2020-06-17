cd253c9834e3d6c4c57b11dd6df3dbc6cda0f1cd
Viren gaming <111402@st-maartenscollege.nl>
2020-06-17 14:12:57 +0000

    Introductie en instructies geschreven, list met de kleuren gemaakt

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+
+print ("  MASTERMIND  \n")
+print ("Welkom bij Mastermind! Het is de bedoeling dat je de kleurencode probeert te raden.\n")
+print ("Voer je code in!\n Je kan de volgende kleuren gebruiken: rood(R), groen(G), blauw(B), geel(Y), wit(W) en paars(P)")
+
+colors = ["R", "G", "B", "Y", "W", "P"]
+attempts = 0
+game = True
