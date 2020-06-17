4e64383a1329434ca43eb1fe8117c0378b1a37ff
Viren gaming <111402@st-maartenscollege.nl>
2020-06-17 14:15:21 +0000

    Game pakt 4 random kleuren uit de list Attempts +1 per beurt (voor de counter) 'Reacties' gemaakt op verkeerde inputs (te lang, kleur zit niet in de list)

--- main.py
+++ main.py
@@ -7,3 +7,19 @@ print ("Voer je code in!\n Je kan de volgende kleuren gebruiken: rood(R), groen(
 colors = ["R", "G", "B", "Y", "W", "P"]
 attempts = 0
 game = True
+
+color_code = random.sample(colors,4)	
+
+while game:
+	correct_color = ""
+	guessed_color = ""
+	player_guess = input().upper()
+	attempts += 1
+	
+	if len(player_guess) != len(color_code):
+		print ("\nDe kleurencode heeft maar 4 kleuren, je invoer is te groot!")
+		continue
+	for i in range(4):
+		if player_guess[i] not in colors:
+			print ("\nDeze kleur wordt niet in het spel gebruikt. Kijk bovenaan bij de instructies welke kleuren gebruikt worden.")
+			continue
